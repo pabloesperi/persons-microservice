d86565afac32cec3f3576f56e387c9866a0f1b3d
pabloesperi <pabloesperi@hotmail.com>
2020-10-10 14:14:32 +0200

    add dependency entity

--- src/main/java/com/proyects/microservices/app/personsmicroservice/PersonsMicroserviceApplication.java
+++ src/main/java/com/proyects/microservices/app/personsmicroservice/PersonsMicroserviceApplication.java
@@ -2,10 +2,12 @@ package com.proyects.microservices.app.personsmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.proyects.microservices.app.commonsmicroservice.models"})
 public class PersonsMicroserviceApplication {
 
 	public static void main(String[] args) {
